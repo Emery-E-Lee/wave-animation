dd96d388d965a11d91f9a42842e6d4c5e9102329
Emery Lee <yiio08@naver.com>
2022-11-04 17:22:18 +0900

    feat #3: Make canvas animation

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,26 +1,46 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
-const Canvas = ({ draw, ...props }) => {
+const Canvas = (props) => {
   const canvasRef = useRef(null);
+  const [context, setContext] = useState(null);
+
+  const draw = (frameCount) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillStyle = "#000000";
+    context.beginPath();
+    context.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    context.fill();
+  };
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) {
-      return;
+    //i.e. value other than null or undefined
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      setContext(ctx);
     }
-    const context = canvas.getContext("2d");
-    if (!context) {
-      return;
+  }, []);
+
+  useEffect(() => {
+    let frameCount = 0;
+    let animationFrameId;
+
+    // Check if null context has been replaced on component mount
+    if (context) {
+      //Our draw came here
+      const render = () => {
+        frameCount++;
+        draw(frameCount);
+        animationFrameId = window.requestAnimationFrame(render);
+      };
+      render();
     }
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [draw, context]);
 
-    draw(context);
-  }, [draw]);
-
-  return (
-    <>
-      <canvas width={props.width} height={props.height} ref={canvasRef} />;
-    </>
-  );
+  return <canvas ref={canvasRef} {...props} />;
 };
 
 export default Canvas;
